575268f3347b70abf1a2d92d734dc96f2abd910e
Brian902 <159546722+Brian902@users.noreply.github.com>
2024-02-13 13:38:19 +0300

    Update Random password.py

--- Random password.py
+++ Random password.py
@@ -1,19 +1,22 @@
-"""
+
 import time
 import random
 import string
+
+
 def generate_password(length="12"):
     seed = int(time.time())
     characters = string.ascii_letters + string.digits + string.punctuation
     random.seed(seed)
 
-    password =''.join(random.choice(characters)for _ in range(length))
+    password = ''.join(random.choice(characters)for _ in range(length))
     return password
 
 password_length = int(input("Enter the desired length for the password: "))
 
 generated_password = generate_password(password_length)
 print(f"Generated Password: {generated_password}")
+
 """
 import secrets
 import string
@@ -27,3 +30,5 @@ password_length = int(input("Enter expected password length: "))
 
 generated_password = generate_password(password_length)
 print(f'The generated password is:{generated_password}')
+
+"""
