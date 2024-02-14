936273273392d89101779eeb64f00e1716ce6fad
Brian902 <159546722+Brian902@users.noreply.github.com>
2024-02-14 21:02:30 +0300

    Update prime numbers.py

--- prime numbers.py
+++ prime numbers.py
@@ -2,7 +2,7 @@ nums = range(1,10000)
 
 def is_prime(num):
     for x in range(2, num):
-        if (num%x) == 0:
+        if (num % x) == 0:
             return False
     return True
 primes = list(filter(is_prime, nums))
